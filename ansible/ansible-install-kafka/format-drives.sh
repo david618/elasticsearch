d0d3c997d79e3f5a95f073231bf0217765442b83
David Jennings <djennings@esri.com>
2019-01-16 12:33:32 -0600

    Added ansible scripts for spark and kafka

---
+++ ansible/ansible-install-kafka/format-drives.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+sudo yum -y install ansible
+
+export ANSIBLE_HOST_KEY_CHECKING=False
+
+ansible-playbook -i hosts --private-key /home/azureuser/dsetestkey playbooks/format-drives.yaml
---
+++ ansible/ansible-install-kafka/install-kafka.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+sudo yum -y install ansible
+
+export ANSIBLE_HOST_KEY_CHECKING=False
+
+ansible-playbook -i hosts --private-key /home/azureuser/dsetestkey  playbooks/install-kafka.yaml
+
