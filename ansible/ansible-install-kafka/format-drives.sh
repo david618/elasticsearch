054a4998a925d52dcb7842f6f08a5eb07f919fa3
David Jennings <djennings@esri.com>
2019-05-08 11:10:58 -0500

    Removed earlier files; will still be in feb2019 branch

--- ansible/ansible-install-kafka/format-drives.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-sudo yum -y install ansible
-
-export ANSIBLE_HOST_KEY_CHECKING=False
-
-ansible-playbook -i hosts --private-key /home/azureuser/dsetestkey playbooks/format-drives.yaml
--- ansible/ansible-install-spark/format-drives.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-sudo yum -y install ansible
-
-export ANSIBLE_HOST_KEY_CHECKING=False
-
-ansible-playbook -i hosts --private-key /home/azureuser/az playbooks/format-data-drive.yaml
-
